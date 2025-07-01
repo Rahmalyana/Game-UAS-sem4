21a85a148ea446c29a696434c79b1cb0d6bd480d
rasyad_tanzilur <tanzilurrahmanrasyad@gmail.com>
2025-07-01 14:52:06 +0700

    Implement new feature for user authentication and improve error handling

---
+++ Assets/Assets/EscToMainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscToMainMenu : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("ESC ditekan, pindah ke main menu");
+            SceneManager.LoadScene("main menu");
+        }
+    }
+}
+
+
