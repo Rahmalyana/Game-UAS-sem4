9fa764a13201aa791b59a6c12c604f6dd9303fb1
Rahmalyana <rahma.06lyana@gmail.com>
2025-07-04 02:57:10 +0700

    musik

---
+++ Assets/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource = GetComponent<AudioSource>();
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (audioSource == null) return;
+
+        if (scene.name.StartsWith("Level-"))
+        {
+            audioSource.Pause(); //  Jangan destroy
+        }
+        else
+        {
+            if (!audioSource.isPlaying)
+                audioSource.Play(); //  Nyalakan kembali
+        }
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
